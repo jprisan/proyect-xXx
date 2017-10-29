7b61c2fb501aeda69dfbfc0f3e9c41cc6d9bbc22
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-10-29 18:11:06 +0100

    Play img

--- lab-victoria-roja/xxx/js/main.js
+++ lab-victoria-roja/xxx/js/main.js
@@ -1,9 +1,5 @@
-
-
-var canvas = document.getElementById('micanvas');
-var ctx = canvas.getContext('2d');
-ctx.fillRect(50, 0, 10, 150);
-ctx.fillStyle = '#cc0000';
-ctx.fillRect(10, 10, 100, 70);
-
-
+window.onload = function() {
+    var canvas = document.getElementById("board");
+    var ctx = canvas.getContext('2d');
+    var board = new Board();
+  
